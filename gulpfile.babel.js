d2715066918120bebcdddb3587b1721a6cde9694
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-03-16 00:17:35 +1100

    Fix gulpfile src path

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 
 gulp.task('default', () => {
-  gulp.src('/src/server/schema/migrations/*')
+  gulp.src('src/server/schema/migrations/*')
       .pipe(babel())
       .pipe(gulp.dest('build/migrations'));
 });
