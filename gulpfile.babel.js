9008335a094bc3cbf3aa14a7cf56862ef67aaac4
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-03-16 00:14:45 +1100

    Add gulpfile to build migrations

---
+++ gulpfile.babel.js
@@ -0,0 +1,8 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+
+gulp.task('default', () => {
+  gulp.src('/src/server/schema/migrations/*')
+      .pipe(babel())
+      .pipe(gulp.dest('build/migrations'));
+}));
