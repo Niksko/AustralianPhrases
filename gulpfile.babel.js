7ebd5b647e2fd2d3d41026ea7c59acd6e1970aac
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-03-16 00:24:04 +1100

    Add building of models to gulpfile
    These are needed by the migrations

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,22 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 
-gulp.task('default', () => {
+gulp.task('migrations', () => {
   gulp.src('src/server/schema/migrations/*')
       .pipe(babel())
-      .pipe(gulp.dest('build/migrations'));
+      .pipe(gulp.dest('build/schema/migrations'));
 });
+
+gulp.task('template', () => {
+  gulp.src('src/server/schema/template.js')
+    .pipe(babel())
+    .pipe(gulp.dest('build/schema'));
+});
+
+gulp.task('word', () => {
+  gulp.src('src/server/schema/word.js')
+    .pipe(babel())
+    .pipe(gulp.dest('build/schema'));
+});
+
+gulp.task('default', ['word', 'template', 'migrations']);
