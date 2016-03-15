bc0195d2246e50049a0f48e6fc6513e0506f94a6
Nikolas M Skoufis <n.skoufis@gmail.com>
2016-03-16 00:41:24 +1100

    Fix typo in downgrade migration

--- src/server/schema/migrations/downgrade.js
+++ src/server/schema/migrations/downgrade.js
@@ -8,23 +8,14 @@ mongoose.connect(process.env.MONGO_URI);
 
 function downgrade(){
   // Drop the word and the template collections
-  mongoose.connection.db.dropCollection('Word', (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      console.log('Successfully dropped the Word collection')
-    }
-  });
-
-  mongoose.connection.db.dropCollection('Template', (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      console.log('Successfully dropped the Template collection')
-    }
-  });
+  mongoose.connection.db.dropCollection('Word')
+   .then(() => {
+      console.log('Successfully dropped the Word collection');
+      mongoose.connection.db.dropCollection('Template');
+   }).then(() => {
+      console.log('Successfully dropped the Template collection');
+      mongoose.disconnect();
+   })
 };
 
 downgrade();
